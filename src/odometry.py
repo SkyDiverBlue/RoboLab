b4818ec885c892bb16f7c847b33f83c8f1c14211
Marine <herbertjans12@gmail.com>
2018-03-02 12:31:25 +0100

    New Change

--- src/odometry.py
+++ src/odometry.py
@@ -1,4 +1,4 @@
 # Suggestion: 	implement odometry as class that is not using the ev3dev.ev3 package
 # 				establish value exchange with main driving class via getters and setters
 
-#Marine
+#MarineL
