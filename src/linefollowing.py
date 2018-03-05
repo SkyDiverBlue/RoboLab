545170ed6c83ec93c18fa166e380479da41f1141
fritzsche <jana.fritzsche96@googlemail.com>
2018-03-05 17:12:14 +0100

    merged version

--- src/linefollowing.py
+++ src/linefollowing.py
@@ -24,36 +24,43 @@ class LineFollowing:
 
 
     def colour_calibration(self): #in this function both luminance will be calibrated 
-        self.white_luminance_value = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
         self.black_luminance_value = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
-    
+        self.motor_left.run_timed(time_sp=500, speed_sp=200) 
+        duty_cycled(20)
+        self.motor_right.run_timed(time_sp=500, speed_sp=200) 
+        duty_cycled(-20)
+    # Startpositionen finden, später genauer definieren !!!
+        self.white_luminance_value = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+        self.motor_left.run_timed(time_sp=500, speed_sp=200) 
+        duty_cycled(-20)
+        self.motor_right.run_timed(time_sp=500, speed_sp=200) 
+        duty_cycled(20)
         self.offset = (self.white_luminance_value + self.black_luminance_value) / 2
 
 
     def line_following(self):
-            actual_luminance = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+        actual_luminance = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+        last_error=0
 
-            last_error=0
+        derivative=0
+        integral=0
 
-            derivative=0
-            integral=0
+        Kp = 1 #Constant for the proportional controller (increase -> sharper turns, decrease -> smoother turns)
+        Ki = 1 #Contant with intergral (summ of running errors)
+        Kd = 1 #Constant with derivative (rate of change of the proportional value)
 
-            Kp = 1 #Constant for the proportional controller (increase -> sharper turns, decrease -> smoother turns)
-            Ki = 1 #Contant with intergral (summ of running errors)
-            Kd = 1 #Constant with derivative (rate of change of the proportional value)
+        error = actual_luminance - offset
+        integral = integral + error
+        derivative = error - last_error
+        error = last_error
 
-            error = actual_luminance - offset
-            integral = integral + error
-            derivative = error - last_error
-            error = last_error
-
-            turn = (Kp*error)+(Ki*integral)+(Kd*derivative)  
+        turn = (Kp*error)+(Ki*integral)+(Kd*derivative)  
     
         
-    if left_touch_sensor.value(self) ==1 or right_touch_sensor.value() == 1:
-            ev3.Sound.speak('obstacle encountered')
-            self.left_motor.stop()
-            self.right_motor.stop() 
+        if left_touch_sensor.value() ==1 or right_touch_sensor.value() == 1:
+                ev3.Sound.speak('obstacle encountered')
+                self.left_motor.stop()
+                self.right_motor.stop() 
     
 colour_calibration(self)
 line_following(self)
