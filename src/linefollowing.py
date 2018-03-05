fedc5662e91dd37ba1cd6ea3f0efd71f3420747f
Marine <cocacola.original4@gmail.com>
2018-03-05 14:09:27 +0100

    start of pip controller code in linefollowing.py -Marine

--- src/linefollowing.py
+++ src/linefollowing.py
@@ -1,32 +1,54 @@
-#Line following program
+#left hand line follower
+#Pid controller
 
 import ev3dev.ev3 as ev3
+import time
 
-def line_following():
-    ev3.Sound.speak('line following has been initialised').wait()
-    
-    left_motor = ev3.LargeMotor('outB')
-    right_motor = ev3.LargeMotor('outC')
-    
-    colour_sensor = ev3.ColorSensor('in2')
-    colour_sensor.mode = 'COL-COLOR'
+ev3.Sound.speak('line following has been initialised').wait()
 
-    left_touch_sensor = ev3.TouchSensor('in1')
-    right_touch_sensor = ev3.TouchSensor('in4')
-   
-    while colour_sensor.value() == 1:
-        left_motor.run_forever(speed_sp=500)
-        right_motor.run_forever(speed_sp=500)                  
+class LineFollowing:
+    def __init__(self):
+        self.left_motor = ev3.LargeMotor('outB')
+        self.right_motor = ev3.LargeMotor('outC')
         
-        if not colour_sensor.value() == 1:
-            ev3.Sound.speak('no black detected')
-            left_motor.stop()
-            right_motor.stop()
+        self.colour_sensor = ev3.ColorSensor('in2')
+        self.colour_sensor.mode = 'RGB-RAW' #Colour sensor is in Raw mode
+
+        self.left_touch_sensor = ev3.TouchSensor('in1')
+        self.right_touch_sensor = ev3.TouchSensor('in4')
+        return
+
+def colour_calibration(): #function to finde the target light value from the colour sensor
+    black = 1
+
+
+
+def line_following(self):
+    target_light_value = target #the light target value witht the white and black from the line
+    actual_light_value = value #actual light value read by the sensor
+
+    Kp = 1 #Constant for the proportional controller (increase -> sharper turns, decrease -> smoother turns)
+    Ki = 1 #Contant with intergral (summ of running errors)
+    Kd = 1 #Constant with derivative (rate of change of the proportional value)
+    error = 0 #
+    integral = 0
+
+    integral = integral + error
+    derivative = error - last_error
+    last_error = error
+
+    turn = (target -  value) * Kp
+
+    motor_speed = Kp * error + Kd * (error - last_error)
+
+
+    
+    
         
     if left_touch_sensor.value() ==1 or right_touch_sensor.value() == 1:
             ev3.Sound.speak('obstacle encountered')
-            left_motor.stop()
-            right_motor.stop() 
+            self.left_motor.stop()
+            self.right_motor.stop() 
     
 
-line_following()
+line_following(self)
