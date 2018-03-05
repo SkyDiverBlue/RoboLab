42b34bdafbd0648083b866fbc4e1a0e219088e02
Marine <cocacola.original4@gmail.com>
2018-03-05 16:07:43 +0100

    Continuation of PID controller code in linefollowing.py, redifinition of variables in main.py -Marine

--- src/linefollowing.py
+++ src/linefollowing.py
@@ -7,42 +7,47 @@ import time
 ev3.Sound.speak('line following has been initialised').wait()
 
 class LineFollowing:
-    def __init__(self):
-        self.left_motor = ev3.LargeMotor('outB')
-        self.right_motor = ev3.LargeMotor('outC')
+    def __init__(self, colour_sensor, motor_list, ts_list):
+        self.left_motor, self.right_motor = motor_list
         
-        self.colour_sensor = ev3.ColorSensor('in2')
+        self.colour_sensor = colour_sensor
         self.colour_sensor.mode = 'RGB-RAW' #Colour sensor is in Raw mode
 
-        self.left_touch_sensor = ev3.TouchSensor('in1')
-        self.right_touch_sensor = ev3.TouchSensor('in4')
+        self.left_touch_sensor, self.right_touch_sensor = ts_list
+
+        self.white_luminance_value = 0
+        self.black_luminance_value = 0
+
+        self.offset = 0
+
         return
 
-def colour_calibration(): #function to finde the target light value from the colour sensor
-    black = 1
 
+def colour_calibration(self): #in this function both luminance will be calibrated 
+    self.white_luminance_value = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+    self.black_luminance_value = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+    
+    self.offset = (self.white_luminance_value + self.black_luminance_value) / 2
 
 
 def line_following(self):
-    target_light_value = target #the light target value witht the white and black from the line
-    actual_light_value = value #actual light value read by the sensor
+    actual_luminance = 0.2126*self.colour_sensor.red+0.7152*self.colour_sensor.green+0.0722*self.colour_sensor.blue
+
+    last_error=0
+
+    derivative=0
+    integral=0
 
     Kp = 1 #Constant for the proportional controller (increase -> sharper turns, decrease -> smoother turns)
     Ki = 1 #Contant with intergral (summ of running errors)
     Kd = 1 #Constant with derivative (rate of change of the proportional value)
-    error = 0 #
-    integral = 0
 
+    error = actual_luminance - offset
     integral = integral + error
     derivative = error - last_error
-    last_error = error
+    error = last_error
 
-    turn = (target -  value) * Kp
-
-    motor_speed = Kp * error + Kd * (error - last_error)
-
-
-    
+    turn = (Kp*error)+(Ki*integral)+(Kd*derivative)  
     
         
     if left_touch_sensor.value() ==1 or right_touch_sensor.value() == 1:
@@ -50,5 +55,5 @@ def line_following(self):
             self.left_motor.stop()
             self.right_motor.stop() 
     
-
+colour_calibration(self)
 line_following(self)
--- src/main.py
+++ src/main.py
@@ -11,7 +11,10 @@ import linefollowing as follow #imports the line following program as follow
 def run():
 # the execution of all code shall be started from within this function
     print('Hello World')
-    follow #executes the line program
+    follow.LineFollowing(colour_sensor = ev3.ColorSensor('in2'),
+                         motor_list = ( ev3.LargeMotor('outB'), ev3.LargeMotor('outC')),
+                         ts_list = (ev3.TouchSensor('in1'), ev3.TouchSensor('in1')))
+    #executes the line program
 
 
 if __name__ == '__main__':
