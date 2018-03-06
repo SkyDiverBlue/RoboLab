79f9a083bf6121b351c3faf83fdcf330bdedd2be
Jona <jona.aliku98@gmail.com>
2018-03-06 16:29:35 +0100

    Not finished and tested yet. Communication

--- src/communication.py
+++ src/communication.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import paho.mqtt.client as mqtt
 # Suggestion: Do not import ev3dev.ev3 module in this file
 
@@ -7,14 +6,35 @@ import paho.mqtt.client as mqtt
 class Communication:
 
     def __init__(self, planet, mqtt_client):
-        """ Initializes communication module, connect to server, subscribe, etc. """
+        #Initializes communication module, connect to server, subscribe, etc.
         self.client = mqtt_client
+        self.planet = planet
         self.client.on_message = self.on_message
+     
+    def connect(client, host, port, username, password,subscribe):
+        client = mqtt.Client(client_id="02",
+                             clean_session=False,
+                             protocol=mqtt.MQTTv31)
+        client.connect = connect
+        client.username_pw_set(username="002", password="someobviouspass") #Set a username and pass for broker authentication.
+        client.connect('robolab.inf.tu-dresden.de', 8883)
+        client.subscribe('explorer/002', qos=1) # subscribe to topic explorer/002
+        client.loop_forever()
+        while True:
+            time.sleep(2)
 
-    def on_message(self, client, data, message):
-        """ Handles the callback if any message arrived """
-        pass
+    def on_message(client, data, message):
+        print('Message received!')
+        #print('content: ' + message.content + ', qos: ' + 
+          #str(message.qos) + ', message: ' + str(message.payload))
+        client.on_message = on_message
+    
 
     def send_message(self, channel, message):
-        """ Sends given message to specified channel """
+        #Sends given message to specified channel
+        #not done
+        print('test')
+       
         pass
+
+
--- src/main.py
+++ src/main.py
@@ -3,15 +3,15 @@
 import ev3dev.ev3 as ev3
 from planet import Direction, Planet
 from communication import Communication
-
+
+import linefollowing as follow #imports the line following program as follow
+
+
+
 def run():
-    # the execution of all code shall be started from within this function
-    print("Hello Marine")ev3.Sound.speak('Welcome to our Robolab project!').wait() #makes robot speakdef run():	#motor right + left	motorleft = ev3.LargeMotor('outB')
-	motorright=ev3.LargeMotor('outC')
-
-	motorleft.run_timed(time_sp=3000, speed_sp=500)	motorright.run_timed(time_sp=3000, speed_sp=500)
-
-
+# the execution of all code shall be started from within this function
+    print('Hello World')
+    follow #executes the line program
 
 
 if __name__ == '__main__':
